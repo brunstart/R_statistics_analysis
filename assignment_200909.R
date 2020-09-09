f0308387a5ce4994864ddf345d44ac9fc01c37dd
Yang Seungmin <53272197+brunstart@users.noreply.github.com>
2020-09-09 19:00:05 +0900

    Add files via upload

---
+++ assignment_200909.R
@@ -0,0 +1,21 @@
+x <- 1:99
+x <- c(1,x[(x %% 3)==0])
+x
+
+y <- 1:99
+y <- c(1,y[(y %% 4)==0])
+y
+
+union(x, y)
+intersect(x, y)
+setdiff(x, y)
+
+data()
+
+air <- airquality
+str(air)
+air[max(air$Wind),]
+mean(air$Ozone, na.rm=T)
+
+str(quakes)
+max(quakes$mag)
